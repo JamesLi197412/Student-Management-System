83b2e47a6d1ffd79a27f64ef7dc539046adb53d8
James Li <zhiyueli.education@gmail.com>
2023-12-19 21:10:46 +0800

    This is my first commit. Init my Student App

---
+++ src/studentDatabaseApp/Student.java
@@ -0,0 +1,25 @@
+package studentDatabaseApp;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String course;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+
+    // Constructor: prompt user to enter student's name and year
+
+    // Generate an ID
+
+    // enroll in course
+
+    // View course balance
+
+    // Pay Tuition
+
+    // Show status
+
+
+}
---
+++ src/studentDatabaseApp/StudentDatabaseApp.java
@@ -0,0 +1,4 @@
+package studentDatabaseApp;
+
+public class StudentDatabaseApp {
+}
